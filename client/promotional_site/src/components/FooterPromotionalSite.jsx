7395742ebcb75407b4f1c3bac8517dc3e0283b1d
seyma <seyma.6ok@hotmail.com>
2023-11-08 14:50:04 +0300

    navbar logo footer

--- client/promotional_site/src/components/FooterPromotionalSite.jsx
+++ client/promotional_site/src/components/FooterPromotionalSite.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom"; // If using React Router for navigation
+import { Typography, Toolbar, Container } from "@mui/material";
 
-export default function FooterPromotionalSite() {
+const FooterPromotionalSite = () => {
+  const footerStyle = {
+    marginTop: "auto",
+    backgroundColor: "#fff",
+    borderTop: "1px solid #ddd",
+  };
+
+  const linkStyle = {
+    textDecoration: "none",
+    color: "#000",
+  };
+
+  const linkHoverStyle = {
+    textDecoration: "underline",
+    color: "linear-gradient(to right, #6C46AE, #A84DB0, #D84FB4)",
+    backgroundClip: "text",
+    textFillColor: "transparent",
+    transition: "all 0.3s ease",
+  };
+
   return (
-    <div>FooterPromotionalSite</div>
-  )
-}
+    <footer style={footerStyle}>
+     <div >
+        <Toolbar style={{ textAlign: "left" }}>
+          <Typography variant="body2" >
+            <Link
+              to="/"
+              style={linkStyle}
+              onMouseEnter={(e) => (e.target.style = linkHoverStyle)}
+              onMouseLeave={(e) => (e.target.style = linkStyle)}
+            >
+              Showcaseify
+            </Link>{" "}
+            © {new Date().getFullYear()}
+          </Typography>
+        </Toolbar>
+    </div>
+    </footer>
+  );
+};
+
+export default FooterPromotionalSite;
--- client/promotional_site/src/components/LogoPromotionalSite.jsx
+++ client/promotional_site/src/components/LogoPromotionalSite.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
-export default function LogoPromotionalSite() {
+export default function LogoPromotionalSite({ responseData }) {
   return (
-    <div>LogoPromotionalSite</div>
-  )
+    <div style={{ textAlign: 'center' }}>
+      <img src={responseData.logo} alt="Logo" style={{ borderRadius: '10%', boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)' }} />
+    </div>
+  );
 }
