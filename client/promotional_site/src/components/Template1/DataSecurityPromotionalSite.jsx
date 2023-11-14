eef15c5420fbb0108f4e0028742905ba9ab68c00
Burak Can <77413569+BurakCanKy@users.noreply.github.com>
2023-11-14 15:53:35 +0300

    Tanıtım sayfası düzeltildi.

--- client/promotional_site/src/components/Template1/DataSecurityPromotionalSite.jsx
+++ client/promotional_site/src/components/Template1/DataSecurityPromotionalSite.jsx
@@ -1,53 +1,40 @@
 import '../Template1/InnovationsSite.css'
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 import './InnovationsSite.css'
 import { motion } from 'framer-motion';
 import styles from '../../styles';
-import { navVariants } from '../../utils/motion';
-
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  display: 'grid',
-  flex: 1,
-
-  margin: 50,
-  fontSize: 25,
-  color: theme.palette.text.secondary,
-}));
+import { navVariants, slideIn } from '../../utils/motion';
 
 const LockIconExample = () => {
   return (
-
+    //https://play.google.com/store/apps/details?id=com.readermobile&hl=tr&gl=US
+    //"https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZjhkaWNlYWl6aTlyaTlibmR3bzU2MTBiaWdxbGJ0cHJ4YTZpYnpuMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/K33m4VQrYC3fQwCkfH/giphy.gif"
+    //https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExeTNpaGEzN2hmOW9xM2ZoZThlY2h5MDl3ZXhtdDBibHppcndmbDl6cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/M8tZRmqgGUxfdCizH8/giphy.gif
     <div>
       <img
-        src="https://pouch.jumpshare.com/preview/jl6Y16CAXHa0N5wUvikBkrMRprqKM-8IJOCBoPTdK26G74yJSnEzdZQNKBYanNcHi8lytStUURG7jA4RH_WbyHN_vqubL_oNIbBfT7gwaSY"
+        src="https://i.ibb.co/qyr4Ts8/Pngtree-cartoon-hand-drawn-network-information-5049321.png"
         alt="Sample GIF"
-        style={{ width: '200px', height: 'auto' }}
+        style={{ width: '400px', height: 'auto' }}
       />
     </div>
   );
 };
 
-export default function DataSecurityPromotionalSite({ responseData }) {
+export default function DataSecurityPromotionalSite({ responseData, colorData }) {
   return (
     <div>
-      <motion.nav variants={navVariants}
+      <motion.nav variants={slideIn('left', 'spring', 0.8, 1)}
         initial="hidden"
         whileInView="show"
         className={`${styles.xPaddings}`}>
         <Grid container>
           <Grid xs={4}>
-            <div className='container'><LockIconExample></LockIconExample></div>
+            <div className='container' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}><LockIconExample></LockIconExample></div>
           </Grid>
           <Grid xs={8}>
-            <div style={{ textAlign: 'center', fontSize: 50 }}>Data Security</div>
+            <div class="header" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', textAlign: 'center', fontSize: 70, color: colorData }}>Data Security</div>
             <div className='container' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>{responseData.dataSecurity}</div>
           </Grid>
         </Grid>
--- client/promotional_site/src/components/Template1/GalleryPromotionalSite.jsx
+++ client/promotional_site/src/components/Template1/GalleryPromotionalSite.jsx
@@ -3,11 +3,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './InnovationsSite.css';
 import CloseIcon from '@mui/icons-material/Close';
+import Grid from '@mui/material/Unstable_Grid2';
 import { motion } from 'framer-motion';
 import styles from '../../styles';
 import { navVariants } from '../../utils/motion';
 
-export default function GalleryPromotionalSite({ responseData }) {
+export default function GalleryPromotionalSite({ responseData, colorData }) {
     const [model, setModel] = useState(false);
     const [tempImgSrc, setTempImgSrc] = useState('')
 
@@ -16,11 +17,15 @@ export default function GalleryPromotionalSite({ responseData }) {
         setTempImgSrc(image)
         setModel(true)
     }
-
     const firstFourImage = []
 
+    for (let i = 0; i < 4; i++) {
+        firstFourImage.push(images[i]);
+    }
+
+
     return (
-        <div>
+        <div >
             <motion.nav variants={navVariants}
                 initial="hidden"
                 whileInView="show"
@@ -29,33 +34,45 @@ export default function GalleryPromotionalSite({ responseData }) {
                     <img src={tempImgSrc} />
                     <CloseIcon onClick={() => setModel(false)} />
                 </div>
-                <div className='gallery'>
+                <div className='gallery' style={{ paddingTop: "5%" }}>
                     <img
                         src={images[0]}
-                        style={{ width: "250px", height: "auto", display: "block", cursor: "pointer" }}
-                        alt='image not found...'
+                        style={{ width: "250px", height: "auto", display: "block", cursor: "pointer", borderBlockColor: colorData }}
+                        alt=''
                         onClick={() => getImage(images[0])}
                     />
                     <img
                         src={images[1]}
-                        style={{ width: "250px", height: "auto", display: "block", cursor: "pointer", paddingTop: "50%" }}
-                        alt='image not found...'
+                        style={{ width: "250px", height: "auto", display: "block", cursor: "pointer", paddingTop: "50%", borderBlockColor: colorData }}
+                        alt=''
                         onClick={() => getImage(images[1])}
                     />
                     <img
                         src={images[2]}
-                        style={{ width: "250px", height: "auto", display: "block", cursor: "pointer" }}
-                        alt='image not found...'
+                        style={{ width: "250px", height: "auto", display: "block", cursor: "pointer", borderBlockColor: colorData }}
+                        alt=''
                         onClick={() => getImage(images[2])}
                     />
                     <img
                         src={images[3]}
-                        style={{ width: "250px", height: "auto", display: "block", cursor: "pointer", paddingTop: "50%" }}
-                        alt='image not found...'
+                        style={{ width: "250px", height: "auto", display: "block", cursor: "pointer", paddingTop: "50%", borderBlockColor: colorData }}
+                        alt=''
+                        onClick={() => getImage(images[3])}
+                    />
+                    <img
+                        src={images[4]}
+                        style={{ width: "250px", height: "auto", display: "block", cursor: "pointer", borderBlockColor: colorData }}
+                        alt=''
+                        onClick={() => getImage(images[3])}
+                    />
+                    <img
+                        src={images[5]}
+                        style={{ width: "250px", height: "auto", display: "block", cursor: "pointer", paddingTop: "50%", borderBlockColor: colorData }}
+                        alt=''
                         onClick={() => getImage(images[3])}
                     />
                 </div>
             </motion.nav>
-        </div>
+        </div >
     )
 }
