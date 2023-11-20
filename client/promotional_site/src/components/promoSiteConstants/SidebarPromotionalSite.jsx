71aa574da93fde2218be22c69eb05e975a36858b
BayramKargin <bayram.kargin@ogr.sakarya.edu.tr>
2023-11-20 16:57:42 +0300

    Navbar-Sidebar

---
+++ client/promotional_site/src/components/promoSiteConstants/SidebarPromotionalSite.jsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+
+export default function TemporaryDrawer({  state, setState, toggleDrawer }) {
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+    </Box>
+  );
+
+  return (
+    <div className={`app-constainer ${state ? 'drawer-open' : ''}`}>
+      {['left'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+            <div>dhjsfajkf</div>
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
