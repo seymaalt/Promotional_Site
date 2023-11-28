33200f5fc4909b8b2d07e39b50e113bddbadb22f
seyma <seyma.6ok@hotmail.com>
2023-11-28 09:50:43 +0300

    route

--- client/promotional_site/src/App.jsx
+++ client/promotional_site/src/App.jsx
@@ -7,6 +7,7 @@ import GlobalProvider from "./context/GlobalProvider.jsx";
 import AuthProvider from './context/AuthProvider.jsx'
 import TextProvider from "./context/TextProvider.jsx";
 import Favorites from './pages/Favorites.jsx'
+import PromotionalSite2 from "./pages/PromotionalSite2.jsx";
 function App() {
   return (
     <AuthProvider>
@@ -16,6 +17,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/promotional-site" element={<PromotionalSite />} />
+              <Route path="/promotional-site2" element={<PromotionalSite2 />} />
               <Route path="/favorites" element={<Favorites />} />
               <Route path="/Login" element={<Login />} />
               <Route path="/Register" element={<Register />} />
