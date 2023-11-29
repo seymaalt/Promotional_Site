c02a2785b0c50a458e76a7053bde70b853711414
seyma <seyma.6ok@hotmail.com>
2023-11-29 11:09:47 +0300

    temp2

--- server/Server.js
+++ server/Server.js
@@ -19,7 +19,7 @@ app.use(
     secret: "cyberwolve",
     resave: true,
     saveUninitialized: true,
-    cookie: { maxAge: 24 * 60 * 60 * 1000 }, // maxAge değerini düzeltin
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }, 
   })
 );
 
