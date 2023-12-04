a7f6416312702acceacaa4b9840c56f34a57ba7b
Burak Can <77413569+BurakCanKy@users.noreply.github.com>
2023-12-04 17:01:14 +0300

    fix

--- client/promotional_site/src/components/Template2/CommentsPromotionalSite.jsx
+++ client/promotional_site/src/components/Template2/CommentsPromotionalSite.jsx
@@ -130,7 +130,7 @@ export default function CommentsPromotionalSite({ responseData }) {
                                     {duzenlemeModuu ? (
                                         <TextareaAutosize
                                             style={{
-                                                width: '100%', background: "#EBEBEB", padding: "40px", marginTop: "0px", maxHeight: "580px", justifyContent: "center", padding: "0px", resize: "none", border: "0px", letterSpacing: "0em", fontSize: `${fontSize}px`, fontFamily: selectedFont, textAlign: `${textAlign}`, fontWeight: "400", color: `${color}`,
+                                                width: '100%', background: "#EBEBEB", padding: "40px", marginTop: "0px", maxHeight: "580px", justifyContent: "center", resize: "none", border: "0px", letterSpacing: "0em", fontSize: `${fontSize}px`, fontFamily: selectedFont, textAlign: `${textAlign}`, fontWeight: "400", color: `${color}`,
                                             }}
                                             multiline
                                             rows={15}
@@ -161,7 +161,7 @@ export default function CommentsPromotionalSite({ responseData }) {
                                     {duzenlemeModuuu ? (
                                         <TextareaAutosize
                                             style={{
-                                                width: '100%', background: "#EBEBEB", padding: "40px", marginTop: "0px", maxHeight: "580px", justifyContent: "center", padding: "0px", resize: "none", border: "0px", letterSpacing: "0em", fontSize: `${fontSize}px`, fontFamily: selectedFont, textAlign: `${textAlign}`, fontWeight: "400", color: `${color}`,
+                                                width: '100%', background: "#EBEBEB", padding: "40px", marginTop: "0px", maxHeight: "580px", justifyContent: "center", resize: "none", border: "0px", letterSpacing: "0em", fontSize: `${fontSize}px`, fontFamily: selectedFont, textAlign: `${textAlign}`, fontWeight: "400", color: `${color}`,
                                             }}
                                             multiline
                                             rows={15}
