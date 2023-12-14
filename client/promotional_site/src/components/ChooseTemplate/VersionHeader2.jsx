d33bb7b55e15c7ff9b1b6a635bd144c41edd0439
Burak Can <77413569+BurakCanKy@users.noreply.github.com>
2023-12-14 14:17:04 +0300

    Choose Screen Version 2 eklendi.

---
+++ client/promotional_site/src/components/ChooseTemplate/VersionHeader2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function header2() {
+  return (
+    <div className="chooseHeader2">
+      <h1>
+        Choose a Style to <span className="coloredText">
+          {" "}Get Started
+        </span>
+      </h1>
+    </div>
+  );
+}
--- client/promotional_site/src/main.jsx
+++ client/promotional_site/src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import HomeV2 from "../src/pages/HomeV2"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HomeV2 />
+    <App />
   </React.StrictMode>,
 )
