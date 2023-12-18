47785c9801d0951a127582e3afff49e379a3d36f
seyma <seyma.6ok@hotmail.com>
2023-12-18 17:49:19 +0300

    login page

--- client/promotional_site/src/pages/LoginV2.jsx
+++ client/promotional_site/src/pages/LoginV2.jsx
@@ -1,36 +1,43 @@
-import Login from "../components/LoginV2/Login"
+import Login from "../components/LoginV2/Login";
 import Grid from "@mui/material/Grid";
-
-
+import image from '../assets/abstract_login.jpg';
 
 export default function BasicGrid() {
   return (
-    <div  style={{
-        backgroundColor: "#161417",
-        height: "100vh",
-      }}>
-    <Grid container spacing={2} sx={{ flexGrow: 1 }}>
-      <Grid xs={2}>
-       
+    <div style={{
+      backgroundColor: "#161417",
+      height: "100vh",
+    }}>
+      <Grid container sx={{ flexGrow: 1 }}>
+        <Grid xs={2}></Grid>
+        <Grid xs={8}>
+          <div style={{
+            borderRadius: '30px',
+            backgroundColor: '#2F2D30',
+            height: "80%",
+            display: "flex",
+            marginTop: "5%"
+          }}>
+            <Grid container sx={{ flexGrow: 1 }}>
+              <Grid xs={6}>
+                <div><Login /></div>
+              </Grid>
+              <Grid xs={6}>      
+                  <img
+                    src={image}
+                    alt="Background"
+                    style={{
+                      width: '100%',
+                      height: "85.7%",
+                      borderRadius: '0px 30px 30px 0px', 
+                    }}
+                  /> 
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+        <Grid xs={2}></Grid>
       </Grid>
-      <Grid xs={8}>
-        <div  style={{
-              borderRadius: '30px',
-              backgroundColor: '#2F2D30',
-              height:"110%",
-              display:"flex",
-              marginTop:"5%"
-            }}>
-                <div></div>
-                <div>  <Login/></div>
-      
-        
-        </div>
-      </Grid>
-    <Grid xs={2}>
-       
-      </Grid>
-    </Grid>
     </div>
   );
 }
