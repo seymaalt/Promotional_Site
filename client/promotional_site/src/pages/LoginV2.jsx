229e91b5ec1753d59d31604a2445b3807e634264
Burak Can <77413569+BurakCanKy@users.noreply.github.com>
2023-12-19 17:48:58 +0300

    Ana sayfa tasarım

--- client/promotional_site/src/pages/LoginV2.jsx
+++ client/promotional_site/src/pages/LoginV2.jsx
@@ -22,15 +22,11 @@ export default function BasicGrid() {
               <Grid xs={6}>
                 <div><Login /></div>
               </Grid>
-              <Grid xs={6}>      
+              <Grid xs={6} className="loginPageImage">      
                   <img
                     src={image}
                     alt="Background"
-                    style={{
-                      width: '100%',
-                      height: "100%",
-                      borderRadius: '0px 30px 30px 0px', 
-                    }}
+                    className="loginPageImage"
                   /> 
               </Grid>
             </Grid>
