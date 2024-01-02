1303c5c92468f1b9ec06c9bb2208144e55134fe9
seyma <seyma.6ok@hotmail.com>
2024-01-02 14:32:49 +0300

    PUBLİSH TEMPLATE1

--- server/routes/contentRoutes.js
+++ server/routes/contentRoutes.js
@@ -2,12 +2,18 @@ const express = require("express");
 const routes = express.Router();
 const {
     getContact,
-    getContactFAV
+    getContactFAV,
+    TempData,
+    publishTemp1
 } = require("../controllers/contentController");
 
 
 routes.route("/").post(getContact);
 
 routes.route("/favorites").post(getContactFAV);
+
+routes.route("/TempData").post(TempData);
+
+routes.route('/publishTemp1/:publishToken').post(publishTemp1);
 
 module.exports = routes;
